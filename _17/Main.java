791313e7e99746f779a41baf155cf23cbc93e4de
Giuseppe <giuseppebron99@gmail.com>
2023-04-24 17:22:15 +0200

    add exercise 16, 17

---
+++ _17/Main.java
@@ -0,0 +1,87 @@
+package javabasics._17;
+
+import java.time.LocalDate;
+import java.time.Month;
+
+public class Main {
+    public static void main(String[] args) {
+        exercise1();
+        exercise3();
+    }
+
+    /**
+     * 1: Write code and fix the program below, you want to accept the questionableFunds
+     * into the account!
+     */
+    private static void exercise1() {
+        System.out.println("Exercise 1: ");
+        int myBankBalanceEuro = 500;
+
+        String suspiciousEmailMessage =
+                """
+                            Hello Sir/Madam,
+                            
+                            I'm a prince here in [redacted] and I need help transferring my billions into 
+                            Italy, please provide your bank account details and I will pay you generously.
+                            
+                            Thanks
+                """;
+
+        long questionableFundsEuro = 5_000_000_000l;
+
+        long myBankBalanceImRich = myBankBalanceEuro + questionableFundsEuro;
+        System.out.println(myBankBalanceImRich);
+
+        //Add the questionable funds to your bank balance and print it out!
+    }
+
+    /**
+     * 2: Given the following information, think about the MOST EFFICIENT (which uses least space)
+     *    type to use to store such data. Why is this the most efficient?
+     *
+     *    long = -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+     *    double = 16 decimal places
+     *    float = 7 decimal places
+     *    int = -2,147,483,648 to 2,147,483,647
+     *    short = -32,768 to 32,768
+     *    byte = -127 to 127
+     *
+     *    2a: Someone's age
+     *    2b: The age of a baby in months
+     *    2c: Money in a hedgefund in euros
+     *    2d: Price of a good in euros on amazon.com
+     *    2e: The exact weight of an apple measured by scientific equipment
+     *    2f: The number of kilometers from any 2 places in the world
+     *
+     *    2a: byte, because people don't live usually more of 127 years.
+     *    2b: short, same before.
+     *    2c: long, somebody can have a lot of money.
+     *    2d: float, we don't need of 16 decimal.
+     *    2e: double, we need to take very accurate weight.
+     *    2f: int, in the world there is not a place that have a distance biggest of 2,147,438,647 kilometer.
+     */
+
+    /**
+     * 3: Use the LOCALDATE type (LocalDate.now()) to print out the current date
+     * <p>
+     * Play around with local date! What can you get it to do?
+     * Use at least the following methods on myDate().
+     * * getDayOfMonth()
+     * * getDayOfWeek()
+     * * getDayOfYear()
+     * * getMonth()
+     * * getMonthValue()
+     * * use isBefore() and isAfter() to compare with other dates, use LocalDate.of(year, month, day) to
+     * create other days
+     */
+    public static void exercise3() {
+        System.out.println("Exercise 3:");
+        System.out.println(LocalDate.now());
+
+        System.out.println(LocalDate.now().getDayOfMonth() + " of " + LocalDate.now().getMonth()+
+                " i will need money but it is " + LocalDate.now().isBefore(LocalDate.of(2023, Month.valueOf("MAY"),27))
+                + " that i take salary " + LocalDate.of(2023, 5,1) + " i don't know how to do." );
+
+
+    }
+}
