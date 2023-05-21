f0518339734d37f1a7d80b6cf9f8604c91ba3f34
Giuseppe <giuseppebron99@gmail.com>
2023-05-21 20:17:05 +0200

    add exercise JavaAdvanced Inheritance

---
+++ javaadvanced/Inheritance/_1/Fish.java
@@ -0,0 +1,26 @@
+package javabasics.javaadvanced.Inheritance._1;
+
+public class Fish extends Animal{
+    private String species;
+    public Fish(double height, double weight, String species) {
+        super(height, weight);
+        this.species = species;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+    public double swimSpeedMetersPerSecond(){
+        return getWeight()*2;
+    }
+    @Override
+    public String toString() {
+        return "Height is: "+ getHeight()+
+                " Weight is: "+ getWeight()+"" +
+                " and species is: "+species;
+    }
+}
